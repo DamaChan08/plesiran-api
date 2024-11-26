4d56d4895708db85712e8e53ab3a7ab815073e43
DamaChan08 <163956366+DamaChan08@users.noreply.github.com>
2024-11-26 22:28:25 +0700

    fix: user login

--- services/auth.service.js
+++ services/auth.service.js
@@ -3,6 +3,7 @@ const _model = require("#models/auth.model");
 const _password = require("#lib/password");
 const { CustomError } = require("#lib/cutomerror");
 const { secretKey } = require("#config/vars");
+const _storage = require("#lib/storage");
 
 const register = async (body) => {
 	body.password = await _password.hashPassword(body.password);
@@ -31,6 +32,7 @@ const login = async (body) => {
 	const { password, ...userData } = user;
 	const token = jwt.sign(userData, secretKey, { expiresIn: "15m" });
 	const refresh_token = jwt.sign(userData, secretKey, { expiresIn: "1h" });
+	userData.profile_image = await _storage.getUrlSigned(userData.profile_image);
 	return {
 		...userData,
 		token,
